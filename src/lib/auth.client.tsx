7897e0f6ada6651f747798bd79ee62125a8f29f5
Eugen Eistrach <eugen.eistrach@konecranes.com>
2024-10-21 20:08:27 +0200

    remove client env as I think its unneeded abstraction and we can do differently later

--- src/lib/auth.client.tsx
+++ src/lib/auth.client.tsx
@@ -5,10 +5,8 @@ import { adminClient, organizationClient } from 'better-auth/client/plugins'
 import { createContext, useContext } from 'react'
 import { getWebRequest } from 'vinxi/http'
 import { authServer } from './auth.server'
-import { env } from './env'
 
 export const authClient = createAuthClient({
-	baseURL: env.VITE_AUTH_URL,
 	plugins: [adminClient(), organizationClient()],
 })
 
