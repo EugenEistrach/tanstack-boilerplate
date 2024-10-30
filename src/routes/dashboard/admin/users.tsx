31042c14a6dd036e732eda547a72a52e3713f1ae
Eugen Eistrach <eugen.eistrach@gmail.com>
2024-10-30 11:01:32 +0100

    navigation simplification

--- src/routes/dashboard/admin/users.tsx
+++ src/routes/dashboard/admin/users.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { UsersList } from '@/features/admin/users-list'
 
-export const Route = createFileRoute('/dashboard/users')({
+export const Route = createFileRoute('/dashboard/admin/users')({
 	component: UsersList,
 })
