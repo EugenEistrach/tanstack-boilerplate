073029401388982bea90e7aa1e46e20cca218e7b
Eugen Eistrach <eugen.eistrach@gmail.com>
2025-01-17 20:38:31 +0100

    refactor: Simplify logger configuration in logger.server.ts
    - Removed conditional transport configuration for non-production environments, streamlining the logger setup.
    - Enhanced clarity and maintainability of the logging configuration by relying solely on the base settings.

--- src/lib/server/logger.server.ts
+++ src/lib/server/logger.server.ts
@@ -5,22 +5,8 @@ const logLevel = process.env['LOG_LEVEL'] ?? 'info'
 const appName = process.env['APP_NAME']
 
 export const logger = pino({
-	transport:
-		appName && nodeEnv !== 'production'
-			? {
-					target: 'pino-pretty',
-					options: {
-						colorize: true,
-						ignore: 'pid,hostname', // Hide noisy fields
-						translateTime: 'HH:MM:ss',
-						messageFormat: '{msg}',
-					},
-				}
-			: undefined,
-
 	base: {
 		app: appName ?? 'app',
-
 		env: nodeEnv,
 	},
 	level: logLevel,
