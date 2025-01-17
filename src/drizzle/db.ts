442a37184f90505faa3cc03f1b3cbdba952901e6
Eugen Eistrach <eugen.eistrach@gmail.com>
2025-01-17 18:25:33 +0100

    refactor: Enhance environment variable handling and update trigger configuration
    - Introduced a new function to parse environment variables in env.server.ts, improving error handling and logging for invalid configurations.
    - Updated trigger.config.ts by removing commented-out code and ensuring clarity in configuration.
    - Added a step in deploy.yml to copy the example environment file, ensuring the environment is set up correctly before deployment.

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -4,15 +4,21 @@ import { drizzle } from 'drizzle-orm/libsql'
 import { schema } from './schemas/schema'
 import { env } from '@/lib/server/env.server'
 
+const disableEmbeddedDb = process.env['DISABLE_EMBEDDED_DB'] === 'true'
+
 let client =
 	env.TURSO_DATABASE_URL && env.TURSO_AUTH_TOKEN
-		? createClient({
-				url: `file:${env.LOCAL_DATABASE_PATH}`,
-
-				authToken: env.TURSO_AUTH_TOKEN,
-				syncUrl: env.TURSO_DATABASE_URL,
-				syncInterval: 60,
-			})
+		? disableEmbeddedDb
+			? createClient({
+					url: env.TURSO_AUTH_TOKEN,
+					syncUrl: env.TURSO_DATABASE_URL,
+				})
+			: createClient({
+					url: `file:${env.LOCAL_DATABASE_PATH}`,
+					authToken: env.TURSO_AUTH_TOKEN,
+					syncUrl: env.TURSO_DATABASE_URL,
+					syncInterval: 60,
+				})
 		: createClient({
 				url: `file:${env.LOCAL_DATABASE_PATH}`,
 			})
--- trigger.config.ts
+++ trigger.config.ts
@@ -1,11 +1,9 @@
-import { additionalPackages } from '@trigger.dev/build/extensions/core'
 import { defineConfig } from '@trigger.dev/sdk/v3'
 
 export default defineConfig({
 	project: 'proj_tlgfryfhwprwupwefzie',
 	runtime: 'node',
 	logLevel: 'log',
-	// Set the maxDuration to 300 seconds for all tasks. See https://trigger.dev/docs/runs/max-duration
 	maxDuration: 300,
 	retries: {
 		enabledInDev: true,
@@ -19,7 +17,6 @@ export default defineConfig({
 	},
 	build: {
 		external: ['libsql'],
-		//extensions: [additionalPackages({ packages: ['@libsql/darwin-arm64'] })],
 	},
 	dirs: ['src/tasks'],
 })
