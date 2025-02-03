aac43f5d1180d12d55f86c6367bdd16e66c63f7f
Eugen Eistrach <eugen.eistrach@gmail.com>
2025-02-03 13:49:18 +0100

    chore(fly): Update Fly.io configuration for deployment optimization

---
+++ src/routes/api/healthcheck.ts
@@ -0,0 +1,32 @@
+import { createAPIFileRoute } from '@tanstack/start/api'
+import { count } from 'drizzle-orm'
+import { db } from '@/drizzle/db'
+import { UserTable } from '@/drizzle/schemas/auth-schema'
+
+export const APIRoute = createAPIFileRoute('/api/healthcheck')({
+	GET: async ({ request }) => {
+		const host =
+			request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+
+		try {
+			// if we can connect to the database and make a simple query
+			// and make a HEAD request to ourselves, then we're good.
+			const [, healthcheck] = await Promise.all([
+				db.select({ count: count() }).from(UserTable),
+				fetch(`${new URL(request.url).protocol}${host}`, {
+					method: 'HEAD',
+					headers: { 'X-Healthcheck': 'true' },
+				}),
+			])
+
+			if (!healthcheck.ok) {
+				return new Response('ERROR', { status: 500 })
+			}
+
+			return new Response('OK')
+		} catch (error: unknown) {
+			console.log('healthcheck ❌', { error })
+			return new Response('ERROR', { status: 500 })
+		}
+	},
+})
